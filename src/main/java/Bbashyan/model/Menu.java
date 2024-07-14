2970d17d95567fa70c309b3759419b9328a39d55
Elandland <68230815+Elandland@users.noreply.github.com>
2024-07-14 19:46:16 +0900

    ✨feat: Restaurant 더미 데이터 삽입

--- src/main/java/Bbashyan/model/Menu.java
+++ src/main/java/Bbashyan/model/Menu.java
@@ -4,6 +4,11 @@ public class Menu {
     private  String name;
     private int price;
 
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
