5ef15a067a52ba22b828b838f3583a74a1d4638a
Yoon <kohyc97@gmail.com>
2024-07-15 10:34:19 +0900

    style: get메소드 -> @Getter

--- src/main/java/Bbashyan/controller/CategoryController.java
+++ src/main/java/Bbashyan/controller/CategoryController.java
@@ -3,13 +3,15 @@ package Bbashyan.controller;
 import Bbashyan.dao.CategoryDao;
 import Bbashyan.model.Category;
 import Bbashyan.view.CategoryView;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.io.IOException;
 import java.util.List;
-
+@Getter
 @Setter
 public class CategoryController {
+
     private CategoryDao categoryDao=new CategoryDao();
     private CategoryView view=new CategoryView(this);
 
@@ -20,6 +22,4 @@ public class CategoryController {
        view.displayCategories(categories);
     }
 
-
-    public RestaurantController getRestaurantController() { return restaurantController;}
 }
